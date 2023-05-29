8b4a20783d09798b61755b52d2017e30cd898ba1
Goleta12 <giogoletiani1999@gmail.com>
2023-05-29 16:24:19 +0400

    saintereso gakvetili

---
+++ day2/გაკვეთილი 2.py
@@ -0,0 +1,61 @@
+knows_programing = True
+
+
+
+#if 10 < 5:
+# print("hello") 
+
+full_name = "gio goletiani"   
+print (len(full_name))
+
+
+print("k" not in full_name)
+
+full_name = "gio goletiani"
+
+#print(full_name[2:7])
+#print(full_name[2:7:2])
+
+#start:finish:step
+
+#print(full_name[:5])
+#print(full_name[5:])
+
+print(full_name[-1])
+print(full_name[-1:-4:-1])
+
+#stringebs aq metodebi 
+# 1) string.it gamoudzaxot metodi string.upper() len(string)
+print(full_name.upper())
+
+
+#name2 = "                NIKA                       "
+#print(name2.upper())
+
+# upper - gadideba     lower - dapataraveba
+
+#print(name2.strip()) # chamochra strep
+
+#print(name2.replace(" ", "#"))
+
+name = "g o"
+
+print(name.replace(" ", "#"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day3/gakvetili3.py
@@ -0,0 +1,28 @@
+know_programin = True
+
+#if 45 > 15:
+#    print("gaaqtiurda 18 wlis zemot")
+
+#name = "gio"
+
+#usarname = "goletiani"
+
+#name = "   gioo   "
+
+#print(name[2:5])
+
+name2 = "tam azi"
+
+print(name2.lower())
+
+print(name2.replace(" ", "#"))
+
+full_name = "g   o r g i"
+
+print(full_name.replace("#"))
+
+
+
+
+
+
